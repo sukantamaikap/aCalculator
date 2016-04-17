c892da510f6e96bc5c83c146eb0d9d01064106b5
sukantamaikap <sukanta.maikap@gmail.com>
2016-04-17 12:58:14 +0530

    adding basic activity code

---
+++ app/src/main/java/mkp/com/acalculator/CalculatorActivity.java
@@ -0,0 +1,16 @@
+package mkp.com.acalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Main activity for calculator project
+ */
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
